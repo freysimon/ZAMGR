17f11261a0940ef97a578eef6172dc6d99aafd7d
freysimon <simon.frey@verbund.com>
2020-05-19 11:40:53 +0200

    Added support for arome in readZAMGGRIB
    Some bug fixes in make.COSEROinput including a wider progressbar

--- R/readZAMGGRIB.r
+++ R/readZAMGGRIB.r
@@ -5,8 +5,8 @@
 #' @import TigR
 #' @export
 #' @param gribfile Absolute path to a gribfile
-#' @param ex Either an \code{\link{extent}} object, NULL, 'alaro', or 'arome'. As of now, 'arome' is not supported, yet.
-#' @param crs Either a \code{\link{crs}} string, NULL, 'alaro', or 'arome'. As of now, 'arome' is not supported, yet.
+#' @param ex Either an \code{\link{extent}} object, NULL, 'alaro', or 'arome'.
+#' @param crs Either a \code{\link{crs}} string, NULL, 'alaro', or 'arome'.
 #' @param wgrib Absolute path to the executable of wgrib. If NULL, wgrib must be installed properly on the system.
 #' @param recnr numeric giving the record number of the respective variable within GRIB file
 #' @param variable character string giving the abbreviated variable within the GRIB file.
@@ -15,15 +15,16 @@
 #' @return A (projected) raster with the extracted information
 #' @seealso For reading INCA files, see \code{\link{readINCABIL}}
 #' @details If ex and/or crs are supplied, the returned raster will be projected. If ex and/or crs are specified as
-#' "alaro" or "arome", the respective extent and crs are used. Note, that as of now, only alaro is supported.
+#' "alaro" or "arome", the respective extent and crs are used.
 #'
 #'     Either recnr or variable must be specified to extract the information of interest.
 #'     If both are given the latter is ignored.
 #'
 #'     This function relies on the package TigR, which is available on \href{https://github.com/freysimon/TigR}{Github}
 #'
-#'     Note that wgrib must be installed on the machine. It can be downloaded from \url{http://www.cpc.ncep.noaa.gov/products/wesley/wgrib.html}
-
+#'     Note that wgrib (64Bit) must be installed on the machine. It can be downloaded from \url{http://www.cpc.ncep.noaa.gov/products/wesley/wgrib.html}
+#'
+#'     ZAMG per default flips their rasters by "y", so the output will be fliped back that the raster is proceted correcly.
 
 readZAMGGRIB <- function(gribfile, ex = NULL, crs = NULL, wgrib = NULL, recnr = NULL, variable = NULL, remove = FALSE){
 
@@ -48,14 +49,28 @@ readZAMGGRIB <- function(gribfile, ex = NULL, crs = NULL, wgrib = NULL, recnr = 
   }
 
   if(!is.null(crs)){
-    if(crs %in% c(tolower("alaro"),toupper("alaro"),"Alaro")){
+    if(crs %in% c(tolower("alaro"),toupper("alaro"),"Alaro",
+                  tolower("arome"),toupper("arome"),"Arome")){
       crs <- "+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs"
     }
   }
 
+  is.alaro <- FALSE
+  is.arome <- FALSE
   if(!is.null(ex)){
-    if(ex %in% c(tolower("alaro"),toupper("alaro"),"Alaro"))
-      ex <- extent(2.0, 2.0+508*0.05999999865889549, 55.11000097543001-446*0.03999999910593033,55.11000097543001)
+    if(ex %in% c(tolower("alaro"),toupper("alaro"),"Alaro")){
+      is.alaro = TRUE
+    }
+    if(ex %in% c(tolower("arome"),toupper("arome"),"Arome")){
+      is.arome = TRUE
+    }
+  }
+
+  if(is.alaro){
+    ex <- extent(2.0, 2.0+508*0.05999999865889549, 55.11000097543001-446*0.03999999910593033,55.11000097543001)
+  }
+  if(is.arome){
+    ex <- extent(5.484000, 22.11600, 42.972000, 51.828000)
   }
 
 
@@ -98,7 +113,7 @@ readZAMGGRIB <- function(gribfile, ex = NULL, crs = NULL, wgrib = NULL, recnr = 
 
   # fill matrix
   for(k in 1:length(rows)){
-    mat1[k,] <- IN[rows[k]:rows0[k],]
+    mat1[k,] <- suppressWarnings(as.numeric(IN[rows[k]:rows0[k],]))
   }
 
   # creating raster from matrix
@@ -110,6 +125,10 @@ readZAMGGRIB <- function(gribfile, ex = NULL, crs = NULL, wgrib = NULL, recnr = 
 
   if(!is.null(crs)){
     crs(ras) <- crs
+  }
+
+  if(any(c(is.arome,is.alaro))){
+    ras <- flip(ras, "y")
   }
 
   if(remove == "success" | remove == TRUE){
